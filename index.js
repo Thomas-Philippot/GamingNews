d4d2b3aabac3ab2ca0f0a5787c4f7e1a6efa2b96
Thomas <thomas.philippot@labo-mylab.fr>
2018-10-03 13:03:30 +0200

    checking API request

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ client.on('ready', () => {
 });
 
 let rule = new schedule.RecurrenceRule();
-rule.hour = 10;
-rule.minute = 55;
+rule.hour = 11;
+rule.minute = 5;
 let j = schedule.scheduleJob(rule, function () {
     console.log('event pushed');
     axios.get('https://newsapi.org/v2/everything?q=Playstation&from=' + now.format() +'&language=fr&sortBy=popularity&apiKey=' + process.env.API_Key)
