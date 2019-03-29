d75a2cb6d4e334d2f2fb8e39ba6d9519f9e82af9
Thomas <thomas.philippot@labo-mylab.fr>
2019-03-29 16:45:17 +0100

    new feature news search

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ client.on('message', message => {
     }
 
     if (message.content.toLowerCase().startsWith('news ')) {
-        message = message.content.replace('news ', '');
+        console.log(message.content.substring(5))
         axios.get('https://newsapi.org/v2/everything', {
             params: {
                 'q': message,
