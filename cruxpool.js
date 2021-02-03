96e103048ed2a21b1c3178c8e275b39759c84d76
thomas <thomas.philippot@labo-mylab.fr>
2021-02-03 12:28:17 +0100

    embed message with eur value

--- cruxpool.js
+++ cruxpool.js
@@ -7,7 +7,7 @@ const cron = require('node-cron');
 cron.schedule('30 17 * * *', () => {
     console.log('running every day');
     client.users.fetch('365160130532081666').then((user) => {
-        sendBalance(user)
+        fetchBalance(user)
     })
 });
 
@@ -15,15 +15,35 @@ const wallet = process.env.ETH_WALLET
 client.on('message', async (message) => {
     if (message.content.toLowerCase() === 'eth balance') {
         const user = await client.users.fetch('365160130532081666');
-        sendBalance(user)
+        fetchBalance(user)
     }
 })
 
-function sendBalance (user) {
+function fetchBalance (user) {
     axios.get(`https://www.cruxpool.com/api/eth/miner/${wallet}/balance`).then((response) => {
         const balance = 0.000000001 * response.data.data.balance
-        user.send(balance).catch((e) => {
-            console.log(e)
+        axios.get('https://pro-api.coinmarketcap.com/v1/tools/price-conversion', {
+            headers: {
+                'X-CMC_PRO_API_KEY': '4781cbd8-52eb-4b88-976d-9e2cf1a5126a'
+            },
+            params: {
+                amount: balance,
+                symbol: "ETH",
+                convert: "EUR"
+            }
+        }).then((response) => {
+            const eur = response.data.data.quote.EUR.price
+            sendBalance(balance, eur, user)
         })
     })
+}
+
+function sendBalance(balance, eur, user) {
+    // send message with ETH amount and EUR real time price
+    const message = new Discord.MessageEmbed()
+        .setTitle('Voici le montant de ton portefeuille sur la pool de minage')
+        .setColor(0x36d44a)
+        .addField('Ethereum', `${balance} ETH`)
+        .addField('Euro', `${eur} €`)
+    user.send(message)
 }
